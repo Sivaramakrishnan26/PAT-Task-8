d4c7a1b5a2f51a5555a25089008044bf77618603
Sivaramakrishnan26 <strsivaram486@gmail.com>
2024-06-23 18:26:57 +0530

    Add files via upload

---
+++ PAT TASK-8.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE IMDB;  -- Create a DB
+USE IMDB;  -- Use the Created DB
+
+CREATE TABLE Movie  -- Create a Table Movie with the below Columns
+(
+MovieID INT AUTO_INCREMENT PRIMARY KEY,  -- Column MovieID as Primary Key with Unique Identifier (Auto Incremented Integer)
+Title VARCHAR(255),
+ReleaseYear INT,
+Language VARCHAR(255) CHECK(Language IN ("Tamil", "Malayalam", "Telugu", "Hindi", "English"))  -- Column Language should have the mentioned values only as a language
+);
+
+CREATE TABLE Media  -- Create a Table Media with the below Columns
+(
+MediaID INT AUTO_INCREMENT PRIMARY KEY,  -- Column MediaID as Primary Key with Unique Identifier (Auto Incremented Integer)
+MediaName VARCHAR(255),
+MovieID INT,
+FOREIGN KEY (MovieID) REFERENCES movie(MovieID),  -- Referencing Movie table with the primary key of the Movie table
+MediaType VARCHAR(6) NOT NULL CHECK (MediaType IN ("Video", "Image"))  -- Column MediaType should have the mentioned values only and it should not be null
+);
+
+CREATE TABLE Genre
+(
+GenreID INT AUTO_INCREMENT PRIMARY KEY,  -- Column GenreID as Primary Key with Unique Identifier (Auto Incremented Integer)
+GenreName VARCHAR(255) NOT NULL CHECK (GenreName IN 
+	("Action", "Comedy", "Romance", "RomCom", "Horror", "Thriller", "SciFi", "Fantasy", "Drama"))  -- Column GenreName should have the mentioned values only as a Genre
+);
+
+CREATE TABLE Users
+(
+UserID INT AUTO_INCREMENT PRIMARY KEY,  -- Column UserID as Primary Key with Unique Identifier (Auto Incremented Integer)
+UserName VARCHAR(255),
+UserEmail VARCHAR(255) CHECK 
+	(UserEmail REGEXP "^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$")  -- Using Regex to check whether the given Email address is valid or not
+);
+
+CREATE TABLE Review
+(
+ReviewID INT AUTO_INCREMENT PRIMARY KEY,  -- Column ReviewID as Primary Key with Unique Identifier (Auto Incremented Integer)
+MovieID INT,
+FOREIGN KEY (MovieID) REFERENCES movie(MovieID),  -- Referencing Movie table with the primary key of the Movie table
+UserID INT,
+FOREIGN KEY (UserID) REFERENCES Users(UserID),  -- Referencing Users table with the primary key of the Users table
+Review VARCHAR(255) CHECK (Review IN ("Good", "Average", "Bad"))
+);
+
+CREATE TABLE Artist
+(
+ArtistID INT AUTO_INCREMENT PRIMARY KEY,  -- Column ArtistID as Primary Key with Unique Identifier (Auto Incremented Integer)
+MovieID INT,
+FOREIGN KEY (MovieID) REFERENCES Movie(MovieID),  -- Referencing Movie table with the primary key of the Movie table
+ArtistName VARCHAR(255),
+DOB DATE
+);
+
+CREATE TABLE Roles
+(
+RoleID INT AUTO_INCREMENT PRIMARY KEY,  -- Column RoleID as Primary Key with Unique Identifier (Auto Incremented Integer)
+ArtistID INT,
+FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID),  -- Referencing Artist table with the primary key of the Artist table
+MovieID INT,
+FOREIGN KEY (MovieID) REFERENCES Movie(MovieID),  -- Referencing Movie table with the primary key of the Movie table
+CharacterName VARCHAR(255),
+RoleType VARCHAR(255) CHECK (RoleType IN
+	("Director", "Producer", "Cinematographer", "Assistant Director", "Male Artist", "Female Artist"))  --   -- Column RoleType should have the mentioned values only as a Role
+);
